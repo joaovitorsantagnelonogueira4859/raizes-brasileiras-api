d3a820c79cd6fe665ad843439ea7666f2576c464
joaovitorsantagnelonogueira4859 <serjones353069br@gmail.com>
2025-06-20 13:04:15 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const AuthController = require("../controllers/auth.controller");
+const ReportsController = require("../controllers/reports.controller");
 
 const routes = Router();
 
 // auth
-routes.post("/auth/register", AuthControllerController.register);
+routes.post("/auth/register", AuthController.register);
 routes.post("/auth/login", AuthController.login);
 // reports
-routes.post("reports/")
+routes.post("/reports", ReportsController.list);
+routes.get("/reports", ReportsController.create);
 
 module.exports = routes;
-
