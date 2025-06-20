97de5c6abec7b439d9669f0510e9396dd0da318c
joaovitorsantagnelonogueira4859 <serjones353069br@gmail.com>
2025-06-20 14:07:24 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,10 @@ const ReportsController = require("../controllers/reports.controller");
 
 const routes = Router();
 
-
 routes.post("/auth/register", AuthController.register);
 routes.post("/auth/login", AuthController.login);
 
-routes.post("/reports", ReportsController.list);
-routes.get("/reports", ReportsController.create);
+routes.post("/reports/create", ReportsController.create);
+routes.get("/reports/list", ReportsController.list);
 
 module.exports = routes;
